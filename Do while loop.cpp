b18091c0b47e6dae67629a4fd0bdc5614ba29f10
UbaidAhmad303 <154446808+UbaidAhmad303@users.noreply.github.com>
2024-01-04 20:16:28 +0500

    Add files via upload

---
+++ Do while loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ; 
+int main ()
+{
+	int M = 0 ;
+    do {
+    	
+    cout << " HI. " << endl ;	
+    	M++ ;
+	}
+	
+	while ( M < 20 ) ;
+	
+	
+	
+	return 0 ;
+}
